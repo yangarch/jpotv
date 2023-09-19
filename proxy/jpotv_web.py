eacef707337e3669fe724080f66a8c0ef78bd04a
ksjung <ksjung@bitmango.com>
2023-09-19 23:03:44 +0900

    Add cookies part

--- proxy/jpotv_web.py
+++ proxy/jpotv_web.py
@@ -119,7 +119,7 @@ for i in CHANNEL_LIST:
             sig = cookie['value']
     time.sleep(2)
 
-new_string = f"chunklist_b1692000.m3u8?Policy={policy}&Signature={sig}&Key-Pair-Id={key_pair}"
+new_string = f"chunklist_b9192000.m3u8?Policy={policy}&Signature={sig}&Key-Pair-Id={key_pair}"
 filename = '/Users/archmacmini/Project/jpotv/proxy/output.txt'  # 원하는 파일 이름으로 변경해주세요.
 
 # 파일에서 내용 읽기
