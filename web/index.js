cf42c328e1d764df16460ffc67b7d9c2d05df3cd
dev-jeong <need9306@gmail.com>
2023-09-22 15:12:09 +0900

    feat(): ui/ux

---
+++ web/index.js
@@ -0,0 +1,9 @@
+var player1 = videojs('video1');
+var player2 = videojs('video2');
+var player3 = videojs('video3');
+var player4 = videojs('video4');
+
+player1.play();
+player2.play();
+player3.play();
+player4.play();
