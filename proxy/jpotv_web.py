aa27daf6928986e31886de07f25bfb2189debbfe
ksjung <ksjung@bitmango.com>
2023-12-18 19:50:26 +0900

    Add highlihgt and extra

--- proxy/jpotv_web.py
+++ proxy/jpotv_web.py
@@ -82,6 +82,12 @@ div_element = browser.find_element(By.CSS_SELECTOR, "div.Program-Channel")
 div_element.click()
 time.sleep(5)
 
+#search main channel
+for i in CHANNEL_LIST:
+    url = f"https://www.spotvnow.co.kr/player?type=channel&id={i}"
+    browser.get(url)
+    time.sleep(5)
+
 # #find key part
 # cookies = {}
 # cookies = browser.get_cookies()
@@ -98,57 +104,41 @@ time.sleep(5)
 urls = []
 #new_string = f"chunklist_b9192000.m3u8?Policy={policy}&Signature={sig}&Key-Pair-Id={key_pair}"    
 
-# 파일 삭제
+#파일 삭제
 with open(f'../result/output.txt', 'w') as file:
     pass
 
-for i in CHANNEL_LIST:
-    url = f"https://www.spotvnow.co.kr/player?type=channel&id={i}"
-    browser.get(url)
-    time.sleep(5)
-
-# #extra channel
-# for i in range(1, 10):
-#     url=f'https://ch0{i}-livescdn.spotvnow.co.kr/ch0{i}/spt0{i}_pc.smil/{new_string}'
-#     browser.get(url)
-#     time.sleep(3)
-
-# #extra channel 2
-# for i in range(10,31):
-#     url=f'https://ch{i}-livescdn.spotvnow.co.kr/ch{i}/spt{i}_pc.smil/{new_string}'
-#     browser.get(url)
-#     time.sleep(3)
-
 #for highlights
-# browser.get(home_url)
-# time.sleep(2)
+browser.get(home_url)
+time.sleep(3)
 
-# highlight_elements_xpaths = []
-# elements = browser.find_elements(By.LINK_TEXT, "//*[contains(text(), '하이라이트')]")
-# for element in elements:
-#     # 각 요소에 대한 XPath를 리스트에 추가
-#     highlight_elements_xpaths.append(browser.execute_script(
-        # "return generateXPath(arguments[0]);", element))
+elements= browser.find_elements(By.XPATH, "//p[contains(text(), '하이라이트')]")
 
-# # XPath를 사용하여 각 요소에 접근하고 클릭
-# for xpath in highlight_elements_xpaths:
-#     try:
-#         # 원래 URL로 돌아가기
-#         browser.get(home_url)
-
-#         # XPath를 사용하여 요소 찾기
-#         element_to_click = browser.find_element(By.XPATH, xpath)
-        
-#         # 요소 클릭
-#         element_to_click.click()
-
-#         time.sleep(3)
-
-#     except:
-#         print("can't find highlights")
+for i in range(0, len(elements)):
+    if i%2 == 0:
+        continue
+    elements[i].click()
+    time.sleep(3)
+    browser.get(home_url)
+    time.sleep(3)
+    elements= browser.find_elements(By.XPATH, "//p[contains(text(), '하이라이트')]")
+    time.sleep(3)
 
 
-filename = '../result/output.txt'
+
+#extra channel
+for i in range(1, 10):
+    url=f'https://ch0{i}-livescdn.spotvnow.co.kr/ch0{i}/spt0{i}_pc.smil//chunklist_b9192000.m3u8'
+    browser.get(url)
+    time.sleep(3)
+
+#extra channel 2
+for i in range(10,31):
+    url=f'https://ch{i}-livescdn.spotvnow.co.kr/ch{i}/spt{i}_pc.smil//chunklist_b9192000.m3u8'
+    browser.get(url)
+    time.sleep(3)
+
+#filename = '../result/output.txt' # path 수정 필요
 
 # # 파일에서 내용 읽기
 # with open(filename, 'r') as file:
@@ -167,12 +157,9 @@ filename = '../result/output.txt'
 # # 수정된 내용으로 파일에 다시 쓰기
 # with open(filename, 'w') as file:
 #    file.writelines(modified_lines)
-
-
+    
 browser.quit()
 
-
-# 접속해서 팝업 끄기,
-# 팝업 끄고 나면 채널 누르기 
-# 첫 채널 들어가서 키 따기
-# 아웃풋 json으로
+    """
+    https://ch01-livescdn.spotvnow.co.kr/ch01/spt01_pc.smil/chunklist_b9192000.m3u8?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHBzOi8vKmxpdmVzY2RuLnNwb3R2bm93LmNvLmtyLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MDE3MDc0MDB9fX1dfQ__&Signature=a-Vn0Jm-MPoVGiNsNKHX---PcEotmiAZgQI73nzqXdj1iRM3LJUBgYcJDZG6ENzc2ZenbC3O5bRwhtRr6E2pjoxV8p0oHkyIJN-crvtD-Ny5sOhF6nPq8gBZGYiQ7Xyq~E6gRo9TREijoPLRwNYnztPY8PklN4CTKaFliussNs1HlR~eewMjR5w0HZUkSb0TZfVfaFW9ScuAgT2xO2pYRXisOO1~yPrXU9Ne2E9D5GROnGVefvTfWa6PYPtlNLiJk~bskf7tRpcSznQrq~rw1l1V7vGmAyJvZNTQcxc~ZZCj3wVeFLYlXJzrkGrai34BU03MBRhE2ElSeuXKBxjubQ__&Key-Pair-Id=APKAI2M6I5EDDXED7H5Q
+    """
