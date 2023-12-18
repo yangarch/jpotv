6e7ee50e54a15327e52fdc659e1fc0c4baaad893
ksjung <ksjung@bitmango.com>
2023-12-18 20:10:54 +0900

    Add master.m3u8

--- proxy/jpotv_proxy.py
+++ proxy/jpotv_proxy.py
@@ -3,9 +3,8 @@ from mitmproxy import ctx, http
 
 def response(flow: http.HTTPFlow) -> None:
     if "playlist.m3u8" in flow.request.url or "master.m3u8" in flow.request.url:
-        if "playlist" in flow.request.url:
-            ctx.log.info(
-                f"Found .m3u8 URL with playlist: {flow.request.url}"
-            )
-            with open("/Users/archmacmini/Project/jpotv/result/output.txt", "a") as f:#path
-                f.write(flow.request.url + "\n")
+        ctx.log.info(
+            f"Found .m3u8 URL with playlist: {flow.request.url}"
+        )
+        with open("/Users/archmacmini/Project/jpotv/result/output.txt", "a") as f:#path
+            f.write(flow.request.url + "\n")
