cadab61505704aea8ad98d6498d6964251b72a80
ksjung <ksjung@bitmango.com>
2024-02-22 13:38:47 +0900

    Fix elements order

--- proxy/jpotv_web.py
+++ proxy/jpotv_web.py
@@ -143,13 +143,15 @@ for element in elements:
 highlight_names = []
 for i in range(0, len(elements)):
     tmp_elements= browser.find_elements(By.XPATH, "//p[contains(text(), '하이라이트')]")
+    if elements[i].text.startswith("하이라이트"):
+        continue
     highlight_names.append(tmp_elements[i].text)
     tmp_elements[i].click()
     time.sleep(3)
     browser.get(home_url)
     time.sleep(3)
     #elements= browser.find_elements(By.XPATH, "//p[contains(text(), '하이라이트')]")
-    #time.sleep(3)
+    time.sleep(3)
 
 filename = '/Users/archmacmini/Project/jpotv/result/output.txt' # path 수정 필요
 with open(filename, 'r') as file:
